1ab7c1d1329a27ab0c7bd3b3a29c3f0c482b0e6b
Crmiv <liujunning.magic@gmail.com>
2015-02-06 23:42:14 +0800

    recom film

--- recomd_test/film.py
+++ recomd_test/film.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
+#easy recommendation
+import recommendations
+
 def loadMovies(path='data/'):
     movies={}
-
-
+    #get movie_id from u.item
+    for line in open('u.item'):
+        (id,title) = line.split('|')[0:2]
+        movies[id] = title
+    
+    #load info
+    prefs={}
+    for line in open('u.data'):
+        (user,movieid,rating,ts) = line.split('\t')
+        prefs.setdefault(user,{})
+        prefs[user][movies[movieid]] = float(rating)
+    return prefs
 
 if __name__ == '__main__':
-    
+    data = loadMovies()
+    print recommendations.getRecommendations(data, '87')[0:30]
+     
 
