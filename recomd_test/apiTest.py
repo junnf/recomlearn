1c2a0f9fafd627cdeb9969a50a2f0d656184437e
Crmiv <liujunning.magic@gmail.com>
2015-02-06 22:14:37 +0800

    one

---
+++ recomd_test/apiTest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from pydelicious import get_popular,get_userposts,get_urlposts
+#tag=''
+#count=1
+#a={}
+#for p in get_popular(tag='programming')[0:count]:
+#    print p['url']
+#    for p2 in get_urlposts(p1['url']):
+#        user = p2['user']
+
+
+
+
+
+
+
+
+
